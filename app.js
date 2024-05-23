dbf8ac492ff705d5e7eb56e9c60449160ac4b8a2
Donovan Gallaway <donovangallaway@gmail.com>
2024-05-23 16:34:46 -0700

    add client

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from "express"
 import dotenv from "dotenv"
+import {messageBuilder, addMessage} from './basicAI'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT
 
+const messages = []
 
-app.get('/', (req,res) => {
-    res.send('Hello world!')
+app.get('/', async (req,res) => {
+    const systemMessage = "You are a helpful assistant."
+    const data = await messages.push(messageBuilder(systemMessage, 'system'))
+    res.status(200).json(data)
+})
+
+app.post('/message', (req,res) => {
+    const history = req.history
+    const message = req.message
 })
 
 app.listen(port, () => {
--- basicAI.js
+++ basicAI.js
@@ -5,23 +5,21 @@ dotenv.config();
 
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
-async function main() {
+const messageBuilder = async (messages, model) =>{
   const completion = await openai.chat.completions.create({
-    messages: [
-      { 
-        role: "system", 
-        content: "You are a helpful assistant for a Game Master. While you are system agnostic, expect to do a lot of Dungeons and Dragons, especially 5e. You are built to help Sarah, a new DM, get comfortable with her first campaign. Please start the conversation with some kind of prompt to get her started in building an encounter." },
-      {
-        role: "system",
-        content: "If you are asked for a monster stat block or a character sheet, please render it as a JSON object with markdown syntax for any descriptions."
-      },
-    ],
-    model: "gpt-4o",
-  });
+    messages: messages,
+    model: model ? model : 'gpt-4o'
+  })
 
-  console.log(completion.choices[0]);
+  return completion.choices[0]
 }
 
-// pull message function out of 
+const addMessage = (message, role) => {
+  return {
+    role: role,
+    message: message
+  }
+}
 
-main();
+
+export {addMessage, messageBuilder}
